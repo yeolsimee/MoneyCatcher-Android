8d05977f9b23fbf6be537a0ececef033641ef580
boring-km <kms0644804@naver.com>
2023-03-30 00:19:08 +0900

    feat/#13: 연/월만 선택하는 커스텀 Spinner Picker 구현

--- app/src/main/java/com/yeolsimee/moneysaving/view/sample/SampleActivity.kt
+++ app/src/main/java/com/yeolsimee/moneysaving/view/sample/SampleActivity.kt
@@ -1,9 +1,13 @@
 package com.yeolsimee.moneysaving.view.sample
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,6 +20,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import com.yeolsimee.moneysaving.R
 import com.yeolsimee.moneysaving.ui.theme.MoneySavingTheme
 import com.yeolsimee.moneysaving.view.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,7 +45,15 @@ class SampleActivity : ComponentActivity() {
                     val state by viewModel.container.stateFlow.collectAsState()
                     val text = remember { mutableStateOf("12") }
 
+                    val dialogState = remember { mutableStateOf(false) }
+                    YearMonthDialog(dialogState)
+
                     Column(verticalArrangement = Arrangement.Center) {
+                        Button(onClick = {
+                            dialogState.value = !dialogState.value
+                        }) {
+                            Text(text = "Date Picker Dialog")
+                        }
                         OutlinedTextField(
                             value = text.value,
                             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Decimal),
@@ -65,6 +80,45 @@ class SampleActivity : ComponentActivity() {
                     }
                     SetSideEffect(this, viewModel)
                 }
+            }
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    @Composable
+    private fun YearMonthDialog(dialogState: MutableState<Boolean>) {
+        if (dialogState.value) {
+            Dialog(onDismissRequest = {
+                dialogState.value = false
+            }) {
+                AndroidView(factory = { context ->
+                    val view =
+                        LayoutInflater.from(context).inflate(R.layout.year_month_picker, null)
+
+                    val yearPicker = view.findViewById<NumberPicker>(R.id.picker_year)
+                    val monthPicker = view.findViewById<NumberPicker>(R.id.picker_month)
+                    val cancelButton = view.findViewById<Button>(R.id.btn_cancel)
+                    val confirmButton = view.findViewById<Button>(R.id.btn_confirm)
+
+                    yearPicker.minValue = 2023
+                    yearPicker.maxValue = 2099
+                    yearPicker.value = 2023
+
+                    monthPicker.minValue = 1
+                    monthPicker.maxValue = 12
+                    monthPicker.value = 3
+
+                    cancelButton.setOnClickListener {
+                        dialogState.value = false
+                    }
+
+                    confirmButton.setOnClickListener {
+                        Toast.makeText(applicationContext, "${yearPicker.value}년 ${monthPicker.value}월", Toast.LENGTH_SHORT).show()
+                        dialogState.value = false
+                    }
+
+                    view
+                })
             }
         }
     }
