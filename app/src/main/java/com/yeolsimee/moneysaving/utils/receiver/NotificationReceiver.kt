9b80c7085cfc4ad75e8ccb3d65333474419543ad
boring-km <kms0644804@naver.com>
2023-06-02 13:40:33 +0900

    fix/#54: 알람이 정확한 시간에 올 수 있게 수정

--- app/src/main/java/com/yeolsimee/moneysaving/utils/receiver/NotificationReceiver.kt
+++ app/src/main/java/com/yeolsimee/moneysaving/utils/receiver/NotificationReceiver.kt
@@ -5,13 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.yeolsimee.moneysaving.App
+import com.yeolsimee.moneysaving.data.db.AlarmDao
 import com.yeolsimee.moneysaving.data.repository.SettingsRepository
+import com.yeolsimee.moneysaving.ui.AlwaysElevenPMAlarmId
 import com.yeolsimee.moneysaving.utils.notification.NotificationHelper
+import com.yeolsimee.moneysaving.utils.notification.RoutineAlarmManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
@@ -23,20 +27,54 @@ class NotificationReceiver : BroadcastReceiver() {
     @Inject
     lateinit var settingsRepository: SettingsRepository
 
+    @Inject
+    lateinit var dao: AlarmDao
+
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.i(App.TAG, "Received: ${intent?.action}, ${intent?.dataString}")
         val notificationHelper = NotificationHelper(context)
 
+        val current = Calendar.getInstance()
+
         if (intent != null && context != null && intent.action == "com.yeolsimee.moneysaving.ROUTINE_ALARM") {
             CoroutineScope(Dispatchers.IO).launch {
                 val alarmTime = intent.getStringExtra("alarmTime") ?: ""
-                Log.i(App.TAG, "saved alarm time: $alarmTime")
+                val alarmId = intent.getIntExtra("alarmId", 0)
+                val routineName = intent.getStringExtra("routineName") ?: ""
+
+                if (alarmId == AlwaysElevenPMAlarmId) {
+                    RoutineAlarmManager.setNextWeek(
+                        context,
+                        current,
+                        routineName,
+                        alarmTime,
+                        alarmId
+                    )
+                } else {
+                    dao.get(alarmId).let {
+                        if (it.isNotEmpty()) {
+                            // 일주일 후에 다시 울리도록 설정
+                            val alarm = it.last()
+                            RoutineAlarmManager.setNextWeek(
+                                context,
+                                current,
+                                alarm.routineName,
+                                alarm.alarmTime,
+                                alarm.alarmId
+                            )
+                        }
+                    }
+                }
+
+                // 설정에서 알람이 꺼져있으면 알림을 보이지 않음
                 settingsRepository.getAlarmState {
                     if (!it) return@getAlarmState
+
+                    // 체크하지 않은 루틴일 때만 알림을 보여줌
                     settingsRepository.getUnCheckedRoutine(alarmTime) { unchecked ->
                         if (unchecked) {
                             Log.i(App.TAG, "alarm not checked: $alarmTime")
-                            val routineName = intent.getStringExtra("routineName") ?: ""
+
                             val nb = notificationHelper.getChannelNotification(
                                 "ROUMO",
                                 routineName,
--- data/src/main/java/com/yeolsimee/moneysaving/data/repository/SettingsRepository.kt
+++ data/src/main/java/com/yeolsimee/moneysaving/data/repository/SettingsRepository.kt
@@ -9,11 +9,18 @@ class SettingsRepository(
     suspend fun getAlarmState(callback: (Boolean) -> Unit) = settingsSource.getAlarmState(callback)
     fun toggleAlarmState() = settingsSource.toggleAlarmState()
     fun setAlarmOn() = settingsSource.setAlarmOn()
-    suspend fun setAlarmOff(callback: (changedState: Boolean) -> Unit) = settingsSource.setAlarmOff(callback)
+    suspend fun setAlarmOff(callback: (changedState: Boolean) -> Unit) =
+        settingsSource.setAlarmOff(callback)
+
     fun setAlarmOffOnce(routine: Routine) = settingsSource.setAlarmOffOnce(routine)
     fun setAlarmOn(routine: Routine) = settingsSource.setAlarmOn(routine)
 
-    fun getUnCheckedRoutine(alarmTime: String, callback: (Boolean) -> Unit) = settingsSource.getUnCheckedRoutine(alarmTime, callback)
-    fun updateCheckRoutineAlarm(beforeAlarmTime: String, afterAlarmTime: String) = settingsSource.updateCheckRoutineAlarm(beforeAlarmTime, afterAlarmTime)
-    suspend fun isFirstInstall(callback: (Boolean) -> Unit) = settingsSource.isFirstInstall(callback)
+    fun getUnCheckedRoutine(alarmTime: String, callback: (Boolean) -> Unit) =
+        settingsSource.getUnCheckedRoutine(alarmTime, callback)
+
+    fun updateCheckRoutineAlarm(beforeAlarmTime: String, afterAlarmTime: String) =
+        settingsSource.updateCheckRoutineAlarm(beforeAlarmTime, afterAlarmTime)
+
+    suspend fun isFirstInstall(callback: (Boolean) -> Unit) =
+        settingsSource.isFirstInstall(callback)
 }
