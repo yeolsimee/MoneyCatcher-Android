8e46e20405475976d16653a930581091bc2fc7e2
boring-km <kms0644804@naver.com>
2023-07-28 14:31:04 +0900

    feat/#77: Groovy to kts, AGP v8.1 Update, remove warnings

--- data/src/main/java/com/yeolsimee/moneysaving/data/db/AppDatabase.kt
+++ data/src/main/java/com/yeolsimee/moneysaving/data/db/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.yeolsimee.moneysaving.data.entity.AlarmEntity
 
-@Database(entities = [AlarmEntity::class], version = 1)
+@Database(entities = [AlarmEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun alarmDao(): AlarmDao
 }
